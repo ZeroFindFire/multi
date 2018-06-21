b3bdeb393ef42c26526014ca7e393494372b6b9a
dimension4 <1174534295@qq.com>
2018-06-21 17:28:40 +0800

    update

--- multi.py
+++ multi.py
@@ -1,5 +1,27 @@
 #coding=utf-8
 
+
+"""
+
+init_push必须调用
+如果你只需要一次性进行所有多线程运行，你只需要调用init_push
+如果你需要分批运行多线程，第一批需要调用init_push，剩下的需要调用push
+
+init_push 和 push 的参数格式都为：
+func, attrs, remain, callback
+	func为需要运行的函数，
+	attrs是传递给func的参数，需要用Multi.attrs函数创建
+	比如你想运行的是 test(6,"name", age=10, healthy = True)
+	attrs需要如此创建：
+		attrs = Multi.attrs([6,"name"],dict(age=10,healthy=True))
+	remain为保留值，将传递给callback，可为空
+	callback为回调函数，可为空，空时调用类的默认函数deal，callback形式为： 
+		callback(response, remain, succeed)
+		response 为函数func的返回值
+		succeed用来判断函数是否运行成功，如果func中抛出异常，则succeed为False
+
+"""
+
 import time
 import threading
 sleep_time=1.0
